99a51184ce59f386b518d4bb207d07f5871e3e19
Eric Crosson <esc@ericcrosson.com>
2016-01-28 13:01:42 -0600

    change of destination gh-pages branch

--- bin/hook-travis-publish-javadoc-github-pages.sh
+++ bin/hook-travis-publish-javadoc-github-pages.sh
@@ -14,7 +14,7 @@ fi
 cd $HOME
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
-git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/stormosson/stormosson.github.io $HOME/gh-pages
+git clone --quiet --branch=master https://${GH_TOKEN}@github.com/stormosson/stormosson.github.io $HOME/gh-pages
 
 # Commit and push changes
 cd $HOME/gh-pages
@@ -22,4 +22,4 @@ git rm -rf *
 cp -Rf $HOME/javadoc-latest/* ./
 git add -f .
 git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-git push -fq origin gh-pages > /dev/null
+git push -fq origin master > /dev/null
