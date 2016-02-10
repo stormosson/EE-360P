8d9451eb24c5f1ec29b4be6da12d4760aa13d9e4
William Mauldin <stormymauldin@utexas.edu>
2016-02-09 21:52:07 -0600

    Got Pset 2 problem 1 up and running

---
+++ assignments/src/pset/two/CyclicBarrier/src/CyclicBarrier.java
@@ -0,0 +1,42 @@
+import java.util.concurrent.locks.ReentrantLock;
+
+public class CyclicBarrier {
+	int parties;
+	int numLeft;
+	private final ReentrantLock lock = new ReentrantLock();
+
+	public CyclicBarrier(int parties) {
+		numLeft = this.parties = parties;
+	}
+
+	public synchronized int await() {
+		// Waits until all parties have invoked await on this barrier.
+		// If the current thread is not the last to arrive then it is
+		// disabled for thread scheduling purposes and lies dormant until
+		// the last thread arrives.
+		// Returns: the arrival index of the current thread, where index
+		// (parties - 1) indicates the first to arrive and zero indicates
+		// the last to arrive.
+		lock.lock();
+		//System.out.println("Locked");
+		int index = -1;
+		try {
+			index = --numLeft;
+			lock.unlock();
+			//System.out.println(numLeft);
+			if (index != 0) {
+				wait();
+			} else {
+				numLeft = parties;
+				notifyAll();
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+
+			//System.out.println("Unlocked");
+		}
+
+		return index;
+	}
+}
