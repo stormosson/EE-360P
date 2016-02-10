1a82fc414223d6c758960029520a587c197376d0
William Mauldin <stormymauldin@utexas.edu>
2016-02-10 14:41:38 -0600

    Updated file so that max chars on one line == 80

--- assignments/src/pset/two/CyclicBarrier/CyclicBarrier.java
+++ assignments/src/pset/two/CyclicBarrier/CyclicBarrier.java
@@ -17,7 +17,8 @@ public class CyclicBarrier {
 	/**
 	 * Construct a CyclicBarrier object
 	 * 
-	 * @param parties the number of threads that have invoked await on this barrier
+	 * @param parties the number of threads that have invoked await on this
+	 * 			      barrier
 	 */
 	public CyclicBarrier(int parties) {
 		numLeft = this.parties = parties;
