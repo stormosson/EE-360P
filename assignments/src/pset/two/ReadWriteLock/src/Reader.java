e82125afeeaa615d8fc219da2d789b90a3116d54
William Mauldin <stormymauldin@utexas.edu>
2016-02-09 21:00:19 -0600

    PASS: pset2 part 2

---
+++ assignments/src/pset/two/ReadWriteLock/src/Reader.java
@@ -0,0 +1,37 @@
+/*
+ * Decompiled with CFR 0_110.
+ * 
+ * Could not load the following classes:
+ *  ReadWriteLock
+ */
+final class Reader
+extends ReaderWriter {
+    public Reader(ReadWriteLock readWriteLock, StringBuffer stringBuffer, int n) {
+        super(readWriteLock, stringBuffer, n);
+    }
+
+    @Override
+    public void beginSession() {
+        try {
+            this.lock.beginRead();
+        }
+        catch (Exception var1_1) {
+            var1_1.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void inSession() {
+        this.sb.append("Reader reads " + this.id + "\n");
+    }
+
+    @Override
+    protected void endSession() {
+        try {
+            this.lock.endRead();
+        }
+        catch (Exception var1_1) {
+            var1_1.printStackTrace();
+        }
+    }
+}
