aae6606460d6370037e374cec43bee0c028473e3
William Mauldin <stormymauldin@utexas.edu>
2016-02-11 17:09:24 -0600

    Re-implemented given test, verified correctness of poll test. I believe our assignment is ready for (re)submission on Canvas

--- assignments/test/pset/two/PriorityQueue/PriorityQueueTester.java
+++ assignments/test/pset/two/PriorityQueue/PriorityQueueTester.java
@@ -7,6 +7,7 @@ package pset.two.PriorityQueue;
  *  PriorityQueue
  */
 
+import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -18,11 +19,11 @@ import pset.two.PriorityQueue.PriorityQueue;
 public class PriorityQueueTester {
 
     @Test
-    public void testConcurrentPriorityQueue() {
+    public void testGivenConcurrentPriorityQueue() {
         ExecutorService executorService = Executors.newCachedThreadPool();
         PriorityQueue priorityQueue = new PriorityQueue(5);
         for (int i = 0; i < 10; ++i) {
-            executorService.submit(new PriorityQueueTesterWorker(priorityQueue));
+            executorService.submit(new PriorityQueueGivenTesterWorker(priorityQueue));
         }
         executorService.shutdown();
     }
@@ -56,14 +57,83 @@ public class PriorityQueueTester {
     	assertEquals(1, priorityQueue.add("Horses", 1));
     	assertEquals("Horspes", priorityQueue.poll());
     }
+    
+    @Test
+    public void testCreatedConcurrentPriorityQueue() {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        PriorityQueue priorityQueue = new PriorityQueue(5);
+        for (int i = 0; i < 10; ++i) {
+            executorService.submit(new PriorityQueueCreatedTesterWorker(priorityQueue));
+        }
+        executorService.shutdown();
+    }
 
 }
 
-class PriorityQueueTesterWorker implements Runnable {
+class PriorityQueueGivenTesterWorker implements Runnable {
+	static final int QUE_SIZE = 5;
+    static final int THREAD_SIZE = 10;
+    static final int THREAD_PAUSE_TIME = 50;
+    static final int PRIORITY_RANGE = 10;
+    private final PriorityQueue que;
+
+    public PriorityQueueGivenTesterWorker(PriorityQueue priorityQueue) {
+        this.que = priorityQueue;
+    }
+
+    @Override
+    public void run() {
+        Random random = new Random();
+        String string = Integer.toString(random.nextInt(10));
+        int n = random.nextInt(10);
+        try {
+            Thread.sleep(random.nextInt(50));
+        }
+        catch (InterruptedException var4_4) {
+            var4_4.printStackTrace();
+        }
+        boolean bl = false;
+        try {
+            if (this.que.add(string, n) >= 0) {
+                System.out.println("Insert " + string + " w/ priority " + n + " SUCEED!");
+                bl = true;
+            } else {
+                System.out.println("Insert " + string + " w/ priority " + n + " FAILED!");
+                bl = false;
+            }
+        }
+        catch (Exception var5_6) {
+            var5_6.printStackTrace();
+        }
+        try {
+            Thread.sleep(random.nextInt(50));
+        }
+        catch (InterruptedException var5_7) {
+            var5_7.printStackTrace();
+        }
+        if (bl) {
+            System.out.println(string + " is located at " + this.que.search(string));
+            try {
+                Thread.sleep(random.nextInt(50));
+            }
+            catch (InterruptedException var5_9) {
+                var5_9.printStackTrace();
+            }
+            try {
+                System.out.println("Pop first: " + this.que.poll());
+            }
+            catch (Exception var5_10) {
+                var5_10.printStackTrace();
+            }
+        }
+    }
+}
+
+class PriorityQueueCreatedTesterWorker implements Runnable {
 
     private final PriorityQueue priorityQueue;
 
-    public PriorityQueueTesterWorker(PriorityQueue queue) {
+    public PriorityQueueCreatedTesterWorker(PriorityQueue queue) {
         this.priorityQueue = queue;
     }
 
