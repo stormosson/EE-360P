cf39b0fe9871aafa1a7ccb0bf90490f477c99485
Eric Crosson <esc@ericcrosson.com>
2016-02-29 21:23:54 -0600

    client reads from stdin and sends data to server

--- assignments/src/pset/three/Client.java
+++ assignments/src/pset/three/Client.java
@@ -5,43 +5,81 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
-  public static void main (String[] args) {
-    String hostAddress;
-    int tcpPort;
-    int udpPort;
 
-    if (args.length != 3) {
-      System.out.println("ERROR: Provide 3 arguments");
-      System.out.println("\t(1) <hostAddress>: the address of the server");
-      System.out.println("\t(2) <tcpPort>: the port number for TCP connection");
-      System.out.println("\t(3) <udpPort>: the port number for UDP connection");
-      System.exit(-1);
+    public static void main (String[] args) {
+
+        String hostAddress;
+        int tcpPort;
+        int udpPort;
+
+        if (args.length != 3 && args.length != 4) {
+            String s = "";
+            s += ("ERROR: Provide 3 arguments\n");
+            s += ("\t(1) <hostAddress>: the address of the server\n");
+            s += ("\t(2) <tcpPort>: the port number for TCP connection\n");
+            s += ("\t(3) <udpPort>: the port number for UDP connection\n");
+            s += ("\t<4> <input>: plaintext file containing input commands\n");
+            System.out.println(s);
+            System.exit(-1);
+        }
+
+        hostAddress = args[0];
+        tcpPort = Integer.parseInt(args[1]);
+        udpPort = Integer.parseInt(args[2]);
+
+        Scanner sc = new Scanner((args.length == 3 ? System.in : args[3]));
+        while(sc.hasNextLine()) {
+
+            String cmd = sc.nextLine();
+            String[] tokens = cmd.split("\\s+");
+
+            /* Honestly, what the hell is this */
+            if (tokens[0].equals("purchase")) {
+            } else if (tokens[0].equals("cancel")) {
+            } else if (tokens[0].equals("search")) {
+            } else if (tokens[0].equals("list")) {
+            } else {
+                System.out.println("ERROR: No such command");
+            }
+
+            boolean udp = tokens[tokens.length-1].toLowerCase().startsWith("u");
+            String message = String.format("%s\n", cmd);
+            String response = "";
+            if (udp) {
+                response = sendUcp(message, udpPort);
+            } else {
+                response = sendTcp(message, tcpPort);
+            }
+            /* TODO: do something with response? */
+        }
     }
 
-    hostAddress = args[0];
-    tcpPort = Integer.parseInt(args[1]);
-    udpPort = Integer.parseInt(args[2]);
+    public String sendUdp(String message, int port) {
 
-    Scanner sc = new Scanner(System.in);
-    while(sc.hasNextLine()) {
-      String cmd = sc.nextLine();
-      String[] tokens = cmd.split(" ");
+        byte[] sendData = message.getBytes();
+        DatagramSocket dsocket = new DatagramSocket();
+        InetAddress address = InetAddress.getByName("localhost");
+        DatagramPacket sendPacket = new DatagramPacket(sendData, 
+                                                       sendData.length,
+                                                       address, port);
+        ssocket.send(sendPacket);
 
-      if (tokens[0].equals("purchase")) {
-        // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
-      } else if (tokens[0].equals("cancel")) {
-        // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
-      } else if (tokens[0].equals("search")) {
-        // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
-      } else if (tokens[0].equals("list")) {
-        // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
-      } else {
-        System.out.println("ERROR: No such command");
-      }
+        byte[] receiveData = new byte[2048];
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, 
+                                                          receiveData.length);
+        ssocket.receive(receivePacket);
+        return new String(receivePacket.getData());
     }
-  }
+
+    public String sendTcp(String message, int port) {
+        
+        Socket ssocket = new Socket("localhost", port);
+        DataOutputStream stdout = 
+            new DataOutputStream(ssocket.getOutputStream());
+        BufferedReader stdin = 
+            new BufferedReader(new InputStreamReader(ssocket.getInputStream()));
+
+        stdout.writeBytes(message);
+        return stdin.readLine();
+    }
 }
