f6714bff2fb9eb64bb280bf4157af67c9c1339d2
Eric Crosson <esc@ericcrosson.com>
2016-04-04 17:08:55 -0500

    beautifying

--- assignments/src/pset/five/Client.java
+++ assignments/src/pset/five/Client.java
@@ -18,7 +18,7 @@ public class Client {
     @SuppressWarnings("resource")
     public static void main (String[] args) {
 
-    	int numAddresses;
+        int numAddresses;
         ArrayList<String> addresses = new ArrayList<String>();
         
 
@@ -35,51 +35,58 @@ public class Client {
 
         numAddresses = Integer.parseInt(args[0]);
         for(int i = 1; i <= numAddresses; i++){
-        	addresses.add(args[i]);
+            addresses.add(args[i]);
         }
 
         Scanner sc = null;
         sc = new Scanner(System.in);
         while(sc.hasNextLine()) {
 
-            String cmd = sc.nextLine();
-            String[] tokens = cmd.split("\\s+");
+            String nextLine = sc.nextLine();
+            String[] tokens = nextLine.split("\\s+");
+            String command = tokens[0];
 
-            if (tokens[0].equals("purchase")) {
-            } else if (tokens[0].equals("cancel")) {
-            } else if (tokens[0].equals("search")) {
-            } else if (tokens[0].equals("list")) {
+            if (command.equals("purchase")) {
+            } else if (command.equals("cancel")) {
+            } else if (command.equals("search")) {
+            } else if (command.equals("list")) {
             } else {
-                System.out.println("ERROR: No such command");
+                System.out.format("No such command: %s", command);
             }
 
-         
-            String message = String.format("%s\n", cmd);
+            String message = String.format("%s\n", nextLine);
             String response = "";
             response = sendTcp(message, addresses);
-            System.out.print(String.format("%s", response));
+            System.out.format("%s", response);
         }
+    }
+
+    public int getPortFromAddress(String str) {
+        return Integer.parseInt(str.split(":")[1]);
+    }
+
+    public String getHostFromAddress(String str) {
+        return str.split(":")[0];
     }
 
     /** Send a message to specified address and port via TCP.
      */
     public static String sendTcp(String message, ArrayList<String> addresses){
-    	int i = 0;
-    	Socket ssocket = new Socket();
-    	while(true){
-    		String strAddress = addresses.get(i).split(":")[0];
-    		int intPort = Integer.parseInt(addresses.get(i).split(":")[1]);
+        int i = 0;
+        Socket ssocket = new Socket();
+        while(true){
+            String strAddress = getHostFromAddress(addresses.get(i));
+            int port = getPortFromAddress(addresses.get(i));
             try {
-            	//TODO: convert to nonblocking socket channel
-				ssocket.connect(new InetSocketAddress(strAddress,intPort), TIMEOUT);
-				stdout = new DataOutputStream(ssocket.getOutputStream());
-		        stdin = new DataInputStream(ssocket.getInputStream());
-		        stdout.writeUTF(message);
-		        return stdin.readUTF();
-			} catch (IOException e1) {
-				i %= i + 1;
-			}
-    	}
-    	
+                //TODO: convert to nonblocking socket channel
+                ssocket.connect(new InetSocketAddress(strAddress,port), TIMEOUT);
+                stdout = new DataOutputStream(ssocket.getOutputStream());
+                stdin = new DataInputStream(ssocket.getInputStream());
+                stdout.writeUTF(message);
+                return stdin.readUTF();
+            } catch (IOException e1) {
+                i %= i + 1;
+            }
+        }
     }
 }
--- assignments/src/pset/five/Server.java
+++ assignments/src/pset/five/Server.java
@@ -11,68 +11,68 @@ import java.util.concurrent.ConcurrentHashMap;
 
 
 class Launcher {
-	
-	/**
-	 * Start a server based on given command line arguments.
-	 */
-	public static void main(String[] args) {
-		int serverID;
-		int numServers;
-		if (args.length != 5) {
-			String s = "";
-			//TODO: change to relevant help string
-			s += ("ERROR: Provide 3 arguments\n");
-			s += ("\t(1) <tcpPort>: the port number for TCP connection\n");
-			s += ("\t(2) <udpPort>: the port number for UDP connection\n");
-			s += ("\t(3) <file>: the file of inventory\n");
-			System.out.println(s);
-			System.exit(-1);
-		}
 
-		serverID = Integer.parseInt(args[0]);
-		numServers = Integer.parseInt(args[1]);
-		String filename = args[2];
-		ArrayList<String> addresses = new ArrayList<String>();
-		for (int i = 3; i < args.length - 2; i++) {
-			addresses.add(args[i]);
-		}
-		
-		Server initServer = null;
-		ArrayList<Thread> servers = new ArrayList<Thread>();
-		/* Initialize -- parse the inventory file */
-		for(int i = 1; i <= numServers; i++){
-			Server s = new Server(i-1, addresses);
-			Thread t = new Thread(s);
-			t.start();
-			servers.add(t);
-			if (initServer == null) {
-				initServer = s;
-			}
-		}
+    /**
+     * Start a server based on given command line arguments.
+     */
+    public static void main(String[] args) {
+        int serverID;
+        int numServers;
+        if (args.length != 5) {
+            String s = "";
+            //TODO: change to relevant help string
+            s += ("ERROR: Provide 3 arguments\n");
+            s += ("\t(1) <tcpPort>: the port number for TCP connection\n");
+            s += ("\t(2) <udpPort>: the port number for UDP connection\n");
+            s += ("\t(3) <file>: the file of inventory\n");
+            System.out.println(s);
+            System.exit(-1);
+        }
 
-		Scanner scan = null;
-		try {
-			scan = new Scanner(new File(filename));
-		} catch (FileNotFoundException e1) {
-			e1.printStackTrace();
-		}
-		while (scan.hasNextLine()) {
-			String[] line = scan.nextLine().split("\\s+");
-			if (line.length != 2)
-				continue;
-			/* Assume: nonmatching inventory lines refer to different items */
-			String item = line[0];
-			String quantity = line[1];
-			ArrayList<String> parameters = new ArrayList<String>(Arrays.asList(item, quantity));
-			initServer.CS("add", parameters);
-			initServer.list();
-		}
+        serverID = Integer.parseInt(args[0]);
+        numServers = Integer.parseInt(args[1]);
+        String filename = args[2];
+        ArrayList<String> addresses = new ArrayList<String>();
+        for (int i = 3; i < args.length - 2; i++) {
+            addresses.add(args[i]);
+        }
 
-		/* Run -- accept incoming requests */
-		// printMap(inventory);
-		
-		
-	}
+        Server initServer = null;
+        ArrayList<Thread> servers = new ArrayList<Thread>();
+        /* Initialize -- parse the inventory file */
+        for(int i = 1; i <= numServers; i++){
+            Server s = new Server(i-1, addresses);
+            Thread t = new Thread(s);
+            t.start();
+            servers.add(t);
+            if (initServer == null) {
+                initServer = s;
+            }
+        }
+
+        Scanner scan = null;
+        try {
+            scan = new Scanner(new File(filename));
+        } catch (FileNotFoundException e1) {
+            e1.printStackTrace();
+        }
+        while (scan.hasNextLine()) {
+            String[] line = scan.nextLine().split("\\s+");
+            if (line.length != 2)
+                continue;
+            /* Assume: nonmatching inventory lines refer to different items */
+            String item = line[0];
+            String quantity = line[1];
+            ArrayList<String> parameters = new ArrayList<String>(Arrays.asList(item, quantity));
+            initServer.CS("add", parameters);
+            initServer.list();
+        }
+
+        /* Run -- accept incoming requests */
+        // printMap(inventory);
+
+
+    }
 }
 
 /**
@@ -80,179 +80,184 @@ class Launcher {
  */
 public class Server implements Runnable{
 
-	private int order_nonce = 1;
-	private Map<String, Integer> inventory = null;
-	private Map<Integer, String> ledger = null;
-	private Map<String, ArrayList<String>> user_orders = null;
-	private String[] command;
-	private InetAddress address;
-	private Integer port; //this is the port that the server "this" listens on
-	private Socket tcpsocket;
+    private int order_nonce = 1;
+    private Map<String, Integer> inventory = null;
+    private Map<Integer, String> ledger = null;
+    private Map<String, ArrayList<String>> user_orders = null;
+    private String[] command;
+    private InetAddress address;
+    private Integer port; //this is the port that the server "this" listens on
+    private Socket tcpsocket;
 
-	public Server(int serverID, ArrayList<String> nodes){
-		inventory = new ConcurrentHashMap<String, Integer>();
-		ledger = new ConcurrentHashMap<Integer, String>();
-		user_orders = new ConcurrentHashMap<String, ArrayList<String>>();
-		String strPort = nodes.get(serverID).split(":")[1];
-		this.port = Integer.parseInt(strPort);
-		new Thread(new TcpListener(port, this)).start();
-	}
-	
-	public void requestCS() {
+    public Server(int serverID, ArrayList<String> nodes){
+        inventory = new ConcurrentHashMap<String, Integer>();
+        ledger = new ConcurrentHashMap<Integer, String>();
+        user_orders = new ConcurrentHashMap<String, ArrayList<String>>();
+        String strPort = nodes.get(serverID).split(":")[1];
+        this.port = Integer.parseInt(strPort);
+        new Thread(new TcpListener(port, this)).start();
+    }
 
-	}
+    public void requestCS() {
 
-	public void releaseCS() {
-		
-	}
-	
-	public void CS(String dispatch, ArrayList<String> parameters) {
-		delta(dispatch, parameters);
-		releaseCS();
-	}
-	
-	public void delta(String dispatch, ArrayList<String> parameters){
-		if("add".equals(dispatch)){
-			String productname = parameters.get(0);
-			String quantity = parameters.get(1);
-			add(productname, quantity);
-		} else if("purchase".equals(dispatch)) {
-			String username = parameters.get(0);
-			String productname = parameters.get(1);
-			String quantity = parameters.get(2);
-			purchase(username, productname, quantity);
-		} else if("cancel".equals(dispatch)) {
-			String orderid = parameters.get(0);
-			cancel(orderid);
-		}
-	}
-	
-	public synchronized void add(String productname, String quantity){
-		int intQuantity = Integer.parseInt(quantity);
-		inventory.put(productname, intQuantity + (inventory.containsKey(productname) ? inventory.get(productname) : 0));
-	}
+    }
 
-	/**
-	 * Handle a purchase event.
-	 */
-	public synchronized String purchase(String username, String productname, String quantity) {
+    public void releaseCS() {
 
-		if (!inventory.containsKey(productname)) {
-			return "Not Available - We do not sell this product";
-		}
-		/* We do have productname in our database */
-		int stock = inventory.get(productname);
-		if (stock - Integer.parseInt(quantity) < 0) {
-			return "Not Available - Not enough items";
-		}
-		/* We do have enough items in stock to complete sale */
-		inventory.put(productname, stock - Integer.parseInt(quantity));
-		Integer orderid = order_nonce++;
+    }
 
-		/* Add user order to ledger */
-		ledger.put(orderid, String.format("%s %s", productname, quantity));
+    public void CS(String dispatch, ArrayList<String> parameters) {
+        delta(dispatch, parameters);
+        releaseCS();
+    }
 
-		/* Relational databases would be great here -- record user orders */
-		ArrayList<String> orders = null;
-		if (!user_orders.containsKey(username)) {
-			orders = new ArrayList<String>();
-			user_orders.put(username, orders);
-		} else {
-			orders = user_orders.get(username);
-		}
-		orders.add(String.format("%s, %s, %s", orderid, productname, quantity));
+    public void delta(String dispatch, ArrayList<String> parameters){
+        if("add".equals(dispatch)){
+            String productname = parameters.get(0);
+            String quantity = parameters.get(1);
+            add(productname, quantity);
+        } else if("purchase".equals(dispatch)) {
+            String username = parameters.get(0);
+            String productname = parameters.get(1);
+            String quantity = parameters.get(2);
+            purchase(username, productname, quantity);
+        } else if("cancel".equals(dispatch)) {
+            String orderid = parameters.get(0);
+            cancel(orderid);
+        }
+    }
 
-		return String.format("You order has been placed, %s %s %s %s", orderid, username, productname, quantity);
-	}
+    /* Use to initialize a live node with initial inventory */
+    public synchronized void add(String productname, String quantity){
+        int intQuantity = Integer.parseInt(quantity);
+        inventory.put(productname, intQuantity +
+                      (inventory.containsKey(productname) ?
+                       inventory.get(productname) : 0));
+    }
 
-	/**
-	 * Handle an order cancellation.
-	 *
-	 * Assumes an order will not be canceled more than once.
-	 */
-	public synchronized String cancel(String orderid) {
+    /**
+     * Handle a purchase event.
+     */
+    public synchronized String purchase(String username, String productname,
+                                        String quantity) {
 
-		Integer ordernum = Integer.parseInt(orderid);
-		if (!ledger.containsKey(ordernum)) {
-			return String.format("%s not found, no such order", ordernum);
-		}
-		/* We recognize the order id, reverse contents of ledger */
-		String[] order = ledger.get(ordernum).split("\\s+");
-		String productname = order[0];
-		Integer quantity = Integer.valueOf(order[1]);
+        if (!inventory.containsKey(productname)) {
+            return "Not Available - We do not sell this product";
+        }
+        /* We do have productname in our database */
+        int stock = inventory.get(productname);
+        if (stock - Integer.parseInt(quantity) < 0) {
+            return "Not Available - Not enough items";
+        }
+        /* We do have enough items in stock to complete sale */
+        inventory.put(productname, stock - Integer.parseInt(quantity));
+        Integer orderid = order_nonce++;
 
-		/*
-		 * Remove the order from the ledger so we cannot 'spawn' infinite items
-		 * through false returns
-		 */
-		ledger.remove(ordernum);
+        /* Add user order to ledger */
+        ledger.put(orderid, String.format("%s %s", productname, quantity));
 
-		inventory.put(productname, quantity + inventory.get(productname));
-		return String.format("Order %s is canceled", ordernum);
-	}
+        /* Relational databases would be great here -- record user orders */
+        ArrayList<String> orders = null;
+        if (!user_orders.containsKey(username)) {
+            orders = new ArrayList<String>();
+            user_orders.put(username, orders);
+        } else {
+            orders = user_orders.get(username);
+        }
+        orders.add(String.format("%s, %s, %s", orderid, productname, quantity));
 
-	/**
-	 * Handle a search request.
-	 *
-	 * Assume canceled orders should still be listed. Assume username exists in
-	 * our database.
-	 */
-	public synchronized String search(String username) {
+        return String.format("You order has been placed, %s %s %s %s",
+                             orderid, username, productname, quantity);
+    }
 
-		if (!user_orders.containsKey(username)) {
-			return String.format("No order found for %s", username);
-		}
+    /**
+     * Handle an order cancellation.
+     *
+     * Assumes an order will not be canceled more than once.
+     */
+    public synchronized String cancel(String orderid) {
 
-		String response = "";
-		for (String order : user_orders.get(username)) {
-			response += String.format("%s\n", order);
-		}
-		return response;
-	}
+        Integer ordernum = Integer.parseInt(orderid);
+        if (!ledger.containsKey(ordernum)) {
+            return String.format("%s not found, no such order", ordernum);
+        }
+        /* We recognize the order id, reverse contents of ledger */
+        String[] order = ledger.get(ordernum).split("\\s+");
+        String productname = order[0];
+        Integer quantity = Integer.valueOf(order[1]);
 
-	/**
-	 * Handle an inventory list query.
-	 */
-	public synchronized String list() {
+        /*
+         * Remove the order from the ledger so we cannot 'spawn' infinite items
+         * through false returns
+         */
+        ledger.remove(ordernum);
 
-		String response = "";
-		for (String item : inventory.keySet()) {
-			response += String.format("%s %d\n", item, inventory.get(item));
-		}
-		return response;
-	}
+        inventory.put(productname, quantity + inventory.get(productname));
+        return String.format("Order %s is canceled", ordernum);
+    }
 
-	/**
-	 * Print a Map object to stdout.
-	 */
-	@SuppressWarnings("unused")
-	private void printMap(Map<Integer, String> map) {
-		System.out.print("{");
-		for (Integer item : map.keySet())
-			System.out.print(String.format("<%s,%s> ", item, map.get(item)));
-		System.out.println("}");
-	}
+    /**
+     * Handle a search request.
+     *
+     * Assume canceled orders should still be listed. Assume username exists in
+     * our database.
+     */
+    public synchronized String search(String username) {
 
-	@Override
-	public void run() {
-		try {
+        if (!user_orders.containsKey(username)) {
+            return String.format("No order found for %s", username);
+        }
 
-			String response = "";
-			String[] args = command[1].split("\\s+");
-			/*send lamport message with command and paramters*/
-			
-			/* else: raise custom exception */
-			respond(String.format("%s\n", response.trim()));
-		} catch (IOException e) {
-			System.err.format("Request aborted: %s", e);
-		}
-		
-	}
-	
-	private void respond(String message) throws IOException {
-		DataOutputStream stdout = new DataOutputStream(tcpsocket.getOutputStream());
-		stdout.writeUTF(message);
-	}
+        String response = "";
+        for (String order : user_orders.get(username)) {
+            response += String.format("%s\n", order);
+        }
+        return response;
+    }
+
+    /**
+     * Handle an inventory list query.
+     */
+    public synchronized String list() {
+
+        String response = "";
+        for (String item : inventory.keySet()) {
+            response += String.format("%s %d\n", item, inventory.get(item));
+        }
+        return response;
+    }
+
+    /**
+     * Print a Map object to stdout.
+     */
+    @SuppressWarnings("unused")
+    private void printMap(Map<Integer, String> map) {
+        System.out.print("{");
+        for (Integer item : map.keySet())
+            System.out.print(String.format("<%s,%s> ", item, map.get(item)));
+        System.out.println("}");
+    }
+
+    @Override
+    public void run() {
+        try {
+
+            String response = "";
+            String[] args = command[1].split("\\s+");
+            /*send lamport message with command and paramters*/
+
+            /* else: raise custom exception */
+            respond(String.format("%s\n", response.trim()));
+        } catch (IOException e) {
+            System.err.format("Request aborted: %s", e);
+        }
+
+    }
+
+    private void respond(String message) throws IOException {
+        DataOutputStream stdout = new DataOutputStream(tcpsocket.getOutputStream());
+        stdout.writeUTF(message);
+    }
 }
 
 /** Handler class to dispatch received commands to the singleton Server.
@@ -266,16 +271,16 @@ class Handler implements Runnable {
     /** Create a handler capable of responding over TCP/UDP.
      */
     public Handler(Socket tcpsocket, Server server) {
-    	this.tcpsocket = tcpsocket;
-    	this.server = server;
-    	DataInputStream stdin;
-		try {
-			stdin = new DataInputStream(tcpsocket.getInputStream());
-			String cmd = stdin.readUTF();
-	        this.command = cmd.trim().split("\\s+", 2);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        this.tcpsocket = tcpsocket;
+        this.server = server;
+        DataInputStream stdin;
+        try {
+            stdin = new DataInputStream(tcpsocket.getInputStream());
+            String cmd = stdin.readUTF();
+            this.command = cmd.trim().split("\\s+", 2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /** Handler's run method, parses received command and relays information to
@@ -287,7 +292,7 @@ class Handler implements Runnable {
 
             String response = "";
             String[] args = command[1].split("\\s+");
-            
+
             if (this.command[0].equals("purchase")) {
                 response = server.purchase(args[0], args[1], args[2]);
             }
@@ -310,9 +315,9 @@ class Handler implements Runnable {
     /** Respond via TCP or UDP to the Client that pinged the Server API.
      */
     private void respond(String message) throws IOException {
-            DataOutputStream stdout =
-                new DataOutputStream(tcpsocket.getOutputStream());
-            stdout.writeUTF(message);
+        DataOutputStream stdout =
+            new DataOutputStream(tcpsocket.getOutputStream());
+        stdout.writeUTF(message);
     }
 }
 
@@ -321,34 +326,34 @@ class Handler implements Runnable {
  */
 class TcpListener implements Runnable {
 
-	int port;
-	Server server;
-	ServerSocket ssocket;
+    int port;
+    Server server;
+    ServerSocket ssocket;
 
-	/**
-	 * Spawn a TCP listener on specified port.
-	 */
-	TcpListener(int port, Server server) {
-		this.port = port;
-		this.server = server;
-		try {
-			ssocket = new ServerSocket(port);
-		} catch (IOException e) {
-			System.err.println(String.format("Server aborted: %s", e));
-		}
-	}
+    /**
+     * Spawn a TCP listener on specified port.
+     */
+    TcpListener(int port, Server server) {
+        this.port = port;
+        this.server = server;
+        try {
+            ssocket = new ServerSocket(port);
+        } catch (IOException e) {
+            System.err.println(String.format("Server aborted: %s", e));
+        }
+    }
 
-	/**
-	 * Listener event loop -- dispatches messages to Handler.
-	 */
-	@Override
-	public void run() {
-		try {
-			while (true) {
-				new Thread(new Handler(ssocket.accept(), server)).start();
-			}
-		} catch (IOException e) {
-			System.err.format("Server aborted: %s", e);
-		}
-	}
+    /**
+     * Listener event loop -- dispatches messages to Handler.
+     */
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                new Thread(new Handler(ssocket.accept(), server)).start();
+            }
+        } catch (IOException e) {
+            System.err.format("Server aborted: %s", e);
+        }
+    }
 }
