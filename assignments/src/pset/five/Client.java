4d314273993c502717e8d5f902e8e243e570a55b
Eric Crosson <esc@ericcrosson.com>
2016-04-05 19:29:38 -0500

    Documented client
    the rest waits until the api is further cemented

--- assignments/src/pset/five/Client.java
+++ assignments/src/pset/five/Client.java
@@ -8,7 +8,7 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/** Client class
+/** Client class -- used to contact the Server with requests.
  */
 public class Client {
 
